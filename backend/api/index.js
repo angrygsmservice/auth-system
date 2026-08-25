9772def49ccec70dce8c045fc4b4c86f3980bcb4
angrygsmservice <angrygsmservice@gmail.com>
2026-08-25 19:04:42 +0500

    Fix production CORS for domain

---
+++ backend/api/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const app = require("../app");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URL);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
