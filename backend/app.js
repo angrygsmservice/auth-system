a5e0da3badb326551ecc87bd11fba6453d2b2b82
angrygsmservice <angrygsmservice@gmail.com>
2026-08-25 18:01:16 +0500

    Fix CORS for angrygsmservice.com

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,7 @@ app.use(
       "http://localhost:3001",
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://auth-frontend-zeta-green.vercel.app",
+      "https://angrygsmservice.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
